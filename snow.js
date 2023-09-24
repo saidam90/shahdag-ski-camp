4fd51158fb3486c5645dae9fc33a5c5d48be8f17
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-09-24 15:34:33 +0400

    Update snow.js animation interval

--- snow.js
+++ snow.js
@@ -5,13 +5,13 @@ var snowStorm = (function (window, document) {
   this.excludeMobile = true; // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) Enable at your own risk.
   this.flakesMax = 128; // Limit total amount of snow made (falling + sticking)
   this.flakesMaxActive = 64; // Limit amount of snow falling at once (less = lower CPU use)
-  this.animationInterval = 50; // Theoretical "miliseconds per frame" measurement. 20 = fast + smooth, but high CPU use. 50 = more conservative, but slower
+  this.animationInterval = 30; // Theoretical "miliseconds per frame" measurement. 20 = fast + smooth, but high CPU use. 50 = more conservative, but slower
   this.useGPU = true; // Enable transform-based hardware acceleration, reduce CPU load.
   this.className = null; // CSS class name for further customization on snow elements
   this.excludeMobile = true; // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) By default, be nice.
   this.flakeBottom = 0; // Integer for Y axis snow limit, 0 or null for "full-screen" snow effect
   this.followMouse = false; // Snow movement can respond to the user's mouse
-  this.snowColor = "#fff"; // Don't eat (or use?) yellow snow.
+  this.snowColor = "#F0EDE5"; // Don't eat (or use?) yellow snow.
   this.snowCharacter = "&bull;"; // &bull; = bullet, &middot; is square on some systems etc.
   this.snowStick = true; // Whether or not snow should "stick" at the bottom. When off, will never collect.
   this.targetElement = null; // element which snow will be appended to (null = document.body) - can be an element ID eg. 'myDiv', or a DOM node reference
